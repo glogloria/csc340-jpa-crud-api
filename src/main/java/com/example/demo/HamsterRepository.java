7fbb64d6bc6b66620a533014c07a8f0e2ad1c4e1
glogloria <theglocode@gmail.com>
2025-10-09 19:21:02 -0400

    Created repository interface

---
+++ src/main/java/com/example/demo/HamsterRepository.java
@@ -0,0 +1,15 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface  HamsterRepository extends JpaRepository<Hamster, Long> {
+
+    List<Hamster> getHamstersByAge(double age);
+
+    @Query(value= "select * from hamsters h where h.age >= 1", nativeQuery = true)
+    List<Hamster> getOlderHamsters(double age);
+
+
+}
